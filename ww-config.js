d5b9e520c1d192663bca5ab442ef62f07529e7b5
Léo Coletta <mail.leo.coletta@gmail.com>
2025-01-14 22:18:50 +0100

    feat: add copilot configuration

--- ww-config.js
+++ ww-config.js
@@ -6,6 +6,22 @@ export default {
             code: 'downloadCSV',
             /* wwEditor:start */
             edit: () => import('./src/components/DownloadCSV.vue'),
+            copilot: {
+                description: 'Downloads data as a CSV file. If the data is an array of objects, it will create columns for all unique keys. If the data is an array of primitive values, it will create a single column.',
+                returns: 'void',
+                schema: {
+                    data: {
+                        type: 'array',
+                        description: 'The data to export to CSV format. Can be an array of objects or primitive values',
+                        bindable: true
+                    },
+                    fileName: {
+                        type: 'string',
+                        description: 'The name of the file to download (without .csv extension)',
+                        bindable: true
+                    }
+                }
+            },
             /* wwEditor:end */
         },
     ],
