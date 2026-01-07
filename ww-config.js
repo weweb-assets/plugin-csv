1760cabb77f383ad39b77cbc2416af1ad9b9001b
Léo Coletta <mail.leo.coletta@gmail.com>
2026-01-07 10:23:36 +0100

    chore(WW-4488): Deprecate download CSV action

--- ww-config.js
+++ ww-config.js
@@ -4,6 +4,8 @@ export default {
         {
             name: 'Download data as CSV',
             code: 'downloadCSV',
+            deprecated: true,
+            deprecatedMessage: 'This action is deprecated. Use the native "Download CSV" action instead.',
             /* wwEditor:start */
             edit: () => import('./src/components/DownloadCSV.vue'),
             /* wwEditor:end */
